81adada03aefaf3df2daffc007cd4f59b55bb607
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-10-23 14:24:26 +0100

    add new card

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,8 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
-import ColumnForm from '../ColumnForm/ColumnForm';
 import { useState } from 'react';
 import shortid from 'shortid';
+import ColumnForm from '../ColumnForm/ColumnForm';
 
 
 const List = () => {
@@ -33,25 +33,58 @@ const List = () => {
         { id: 1, title: 'Fifa 2023' },
         { id: 2, title: 'Skyrim' }
       ]
-    }
+    },
   ]);
 
   const addColumn = newColumn => {
-    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
+    setColumns([...columns,
+      { 
+        id: shortid(), 
+        title: newColumn.title, 
+        icon: newColumn.icon, 
+        cards: [],
+      },
+    ]);
+  };
+
+  const addCard = (newCard, columnId) => {
+    const columnsUpdated = columns.map((column) => {
+      if (column.id === columnId)
+        return {
+          ...column,
+          cards: [...column.cards, { id: shortid(), title: newCard.title }],
+        };
+      else return column;
+    });
+
+    setColumns(columnsUpdated);
   };
 
   return (
     <div className={styles.list}>
       <header className={styles.header}>
-          <h2 className={styles.title}>Things to do<span>soon</span></h2>
+        <h2 className={styles.title}>
+          Things to do <span>soon</span>
+        </h2>
       </header>
-      <p className={styles.description}>Interesting things I want to check out</p>
+      <p className={styles.description}>
+        Interesting things I want to check out
+      </p>
       <section className={styles.columns}>
-        {columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} />)}
+        {columns.map((column) => (
+          <Column
+            key={column.id}
+            id={column.id}
+            title={column.title}
+            icon={column.icon}
+            cards={column.cards}
+            action={addCard}
+          />
+        ))}
       </section>
       <ColumnForm action={addColumn} />
     </div>
-  );  
+  ); 
 };
 
 
