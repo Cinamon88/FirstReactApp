ebcda82d47495c28cfe358e28f3cab3242ff2498
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-05 22:28:38 +0000

    add useSelector

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,22 +1,28 @@
 import styles from './Column.module.scss'
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = (props) => {
-    return (
-      <article className={styles.column}>
-        <h2 className={styles.title}>
-          <span className={styles.icon + ' fa fa-' + props.icon}></span>
-          {props.title}
-        </h2>
-        <ul className={styles.cards}>
-          {props.cards.map((card) => (
-            <Card key={card.id} title={card.title} />
-          ))}
-        </ul>
-        <CardForm columnId={props.id} action={props.action} />
-      </article>
-    );
-  };
+
+  const cards = useSelector(state => state.cards);
+
+  return (
+    <article className={styles.column}>
+      <h2 className={styles.title}>
+        <span className={styles.icon + ' fa fa-' + props.icon}></span>
+        {props.title}
+      </h2>
+      <ul className={styles.cards}>
+        {cards.map(cards =>
+          <Card
+            key={cards.id}
+            {...cards} />
+        )}
+      </ul>
+      <CardForm columnId={props.id} action={props.action} />
+    </article>
+  );
+};
 
 export default Column;
