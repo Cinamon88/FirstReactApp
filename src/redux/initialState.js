192a916a64d9bb858f0e2d1223f7417c76c1e740
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-06 21:12:37 +0000

    add shortid

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -20,9 +20,9 @@ const initialState = {
     cards: [
       { id: 1, columnId: 1, title: 'This is Going to Hurt' },
       { id: 2, columnId: 1, title: 'Interpreter of Maladies' },
-      { id: 3, columnId: 2, title: 'Harry Potter' },
+      { id: 3, columnId: 2, title: 'Lost in Space' },
       { id: 4, columnId: 2, title: 'Star Wars' },
-      { id: 5, columnId: 3, title: 'The Witcher' },
+      { id: 5, columnId: 3, title: 'FIFA 2023' },
       { id: 6, columnId: 3, title: 'Skyrim' }
     ]
   
