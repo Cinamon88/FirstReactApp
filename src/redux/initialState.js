7910c86387727494785231ef225a8536ba730944
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-10 09:57:43 +0100

    add searchString

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -24,7 +24,9 @@ const initialState = {
       { id: 4, columnId: 2, title: 'Star Wars' },
       { id: 5, columnId: 3, title: 'FIFA 2023' },
       { id: 6, columnId: 3, title: 'Skyrim' }
-    ]
+    ],
+
+    searchString: '', 
   
   };
   
