3996bc9c82fdc573da198e00ae3124c3d2173a94
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-12 21:16:26 +0100

    add action creator addColumn

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,13 +2,14 @@ import styles from './ColumnForm.module.scss';
 import {useState} from 'react';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
+import { addColumn } from '../../redux/store';
 
 const ColumnForm = props => {
     const dispatch = useDispatch();
 
     const handleSubmit = e => {
         e.preventDefault();
-        dispatch({ type: 'ADD_COLUMN', payload: {title, icon}});
+        dispatch(addColumn({ title, icon }));
         setTitle('');
         setIcon('');
     }
