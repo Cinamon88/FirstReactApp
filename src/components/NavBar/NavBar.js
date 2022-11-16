91a0258cc33c31c02a2f1132c1b256a468cf9dba
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-16 15:00:51 +0100

    remove BrowserRouter

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,19 @@
 import Container from '../Container/Container';
 import styles from './NavBar.module.scss';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav className={styles.NavBar}>
             <Container>
-                <BrowserRouter>
-                    <div className={styles.links}>
-                        <Link to="/"><i className={styles.icon + " fa fa-tasks"} aria-hidden="true"></i></Link>
-                        <ul>
-                            <Link to="/"><li>Home</li></Link>
-                            <Link to="/favorite"><li>Favorite</li></Link>
-                            <Link to="/about"><li>About</li></Link>
-                        </ul>
-                    </div>
-                </BrowserRouter>
+                <div className={styles.links}>
+                    <Link to="/"><i className={styles.icon + " fa fa-tasks"} aria-hidden="true"></i></Link>
+                    <ul>
+                        <Link to="/"><li>Home</li></Link>
+                        <Link to="/favorite"><li>Favorite</li></Link>
+                        <Link to="/about"><li>About</li></Link>
+                    </ul>
+                </div>  
             </Container>
         </nav>
     )
