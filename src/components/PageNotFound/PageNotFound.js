d5ca4eec700433c17a25f6a4e544d330910fe60b
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-16 15:42:15 +0100

    add new component PageNotFound

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <PageTitle>404 Not found!</PageTitle>
+        </div>
+    );
+};
+
+export default PageNotFound;
