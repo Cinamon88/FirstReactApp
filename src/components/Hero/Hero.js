e849b3258f01697f1f9c69a66779a231a5fbb4ca
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-16 14:27:23 +0100

    fix typo1

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import styles from './Hero.module.scss';
 const Hero = () => {
     return (
       <div className={styles.hero}>
-        <PageTitle>My first React App</PageTitlegit add >
+        <PageTitle>My first React App</PageTitle>
         <p className={styles.subtitle}>A simple to-do app, with lists, columns and card</p>
       </div>
     );
