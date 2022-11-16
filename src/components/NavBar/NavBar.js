66b16092dbb3f84a7ffaf8dd238270e7bd2792cf
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-16 13:45:14 +0100

    add 'Link to' & style

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,21 @@
 import Container from '../Container/Container';
 import styles from './NavBar.module.scss';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav className={styles.NavBar}>
             <Container>
-                <div className={styles.links}>
-                    <i className={styles.icon + " fa fa-tasks"} aria-hidden="true"></i>
-                    <ul>
-                        <li>Home</li>
-                        <li>Favorite</li>
-                        <li>About</li>
-                    </ul>
-                </div>
+                <BrowserRouter>
+                    <div className={styles.links}>
+                        <Link to="/"><i className={styles.icon + " fa fa-tasks"} aria-hidden="true"></i></Link>
+                        <ul>
+                            <Link to="/"><li>Home</li></Link>
+                            <Link to="/favorite"><li>Favorite</li></Link>
+                            <Link to="/about"><li>About</li></Link>
+                        </ul>
+                    </div>
+                </BrowserRouter>
             </Container>
         </nav>
     )
