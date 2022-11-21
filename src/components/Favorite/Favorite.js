1f9e2a87a1f0660708e93edddcbcf55064dd8741
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-21 16:13:45 +0100

    formatting

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -5,33 +5,32 @@ import PageTitle from '../PageTitle/PageTitle';
 import Card from '../Card/Card';
 
 const Favorite = () => {
-    const favoriteCards = useSelector(getFavoriteCard);
-    if (favoriteCards.length === 0) {
-      return (
-        <>
-          <PageTitle>No favorite cards</PageTitle>
-        </>
-      );
-    }
-
+  const favoriteCards = useSelector(getFavoriteCard);
+  if (favoriteCards.length === 0) {
     return (
       <>
-        <PageTitle>Favorite</PageTitle>
-          <div className={styles.column}>
-            <ul className={styles.cards}>
-              {favoriteCards.map((favoriteCard) => (
-                <Card
-                  key={favoriteCard.id}
-                  cardId={favoriteCard.id}
-                  title={favoriteCard.title}
-                  isFavorite={favoriteCard.isFavorite}
-                />
-              ))}
-            </ul>
-          </div>
+        <PageTitle>No favorite cards</PageTitle>
       </>
     );
+  }
 
+  return (
+    <>
+      <PageTitle>Favorite</PageTitle>
+      <div className={styles.column}>
+        <ul className={styles.cards}>
+          {favoriteCards.map((favoriteCard) => (
+            <Card
+              key={favoriteCard.id}
+              cardId={favoriteCard.id}
+              title={favoriteCard.title}
+              isFavorite={favoriteCard.isFavorite}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Favorite;
