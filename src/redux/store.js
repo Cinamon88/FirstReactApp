7fc73b3a73567783c7c30b980afd12326566b90a
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-21 14:17:40 +0100

    add selector, action creator & reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,20 +4,24 @@ import shortid from 'shortid';
 import strContains from '../utils/strContains';
 
 //selectors
-export const getFilteredCards = ({ cards, searchString}, columnId) => cards
-  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+export const getFilteredCards = ({ cards, searchString}, columnId) => 
+  cards.filter(card => card.columnId === columnId && strContains(card.title, searchString));
 export const getAllColumns = (state) => state.columns;
-export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId); 
+export const getListById = ({ lists }, listId) => lists.find(list => 
+  list.id === listId); 
 export const getColumnsByList = ({ columns }, listId) => columns
  .filter((column) => column.listId === listId);
 export const getAllLists = (state) => state.lists;
 export const searchInputValue = (state) => state.searchString;
+export const getFavoriteCard = (state) => state.cards.filter((card) => 
+  card.isFavorite === true);
 
 // action creators
 export const addList = (payload) => ({ type: 'ADD_LIST', payload });
 export const addColumn = (payload) => ({ type: 'ADD_COLUMN', payload });
 export const addCard = (payload) => ({ type: 'ADD_CARD', payload });
 export const updateSearchString = (payload) => ({ type: 'UPDATE_SEARCHSTRING', payload, });
+export const toggleCardFavorite = (payload) => ({ type: 'TOGGLE_CARD_PAYLOAD', payload, });
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -42,6 +46,12 @@ const reducer = (state, action) => {
     case 'UPDATE_SEARCHSTRING':
       return { 
         ...state, searchString: action.payload 
+      };
+
+    case 'TOGGLE_CARD_FAVORITE':
+      return { 
+        ...state, cards: state.cards.map(card =>
+        (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card) 
       };
 
     default:
