f0e35046429e8d145ece7f44d8ee200ab986a424
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-21 12:44:48 +0100

    show state.searchString

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,17 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
-import { updateSearchString } from '../../redux/store';
+import { searchInputValue, updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
+    const currentSearchString = useSelector(searchInputValue);
+    const [searchValue, setSearchValue] = useState(currentSearchString);
     const dispatch = useDispatch();
-    const [searchValue, setSearchValue] = useState('');
+    
 
-    const handleSubmit = e => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(updateSearchString(searchValue));
     };
