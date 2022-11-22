b969fb9b8947a41f995de792fc1b303ba73d5c36
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-22 16:51:18 +0100

    move selectors & creator to listsRedux

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,17 +10,13 @@ import searchStringReducer from './searchStringRedux';
 export const getFilteredCards = ({ cards, searchString}, columnId) => 
   cards.filter(card => card.columnId === columnId && strContains(card.title, searchString));
 export const getAllColumns = (state) => state.columns;
-export const getListById = ({ lists }, listId) => lists.find(list => 
-  list.id === listId); 
 export const getColumnsByList = ({ columns }, listId) => columns
  .filter((column) => column.listId === listId);
-export const getAllLists = (state) => state.lists;
 export const searchInputValue = (state) => state.searchString;
 export const getFavoriteCard = (state) => state.cards.filter((card) => 
   card.isFavorite === true);
 
 // action creators
-export const addList = (payload) => ({ type: 'ADD_LIST', payload });
 export const addColumn = (payload) => ({ type: 'ADD_COLUMN', payload });
 export const addCard = (payload) => ({ type: 'ADD_CARD', payload });
 export const updateSearchString = (payload) => ({ type: 'UPDATE_SEARCHSTRING', payload, });
