1534bd46619bea7a60c13f567907dc2b8c8954f4
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-22 21:47:21 +0100

    move selectors & creators to cardsRedux

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,6 +1,6 @@
 import styles from './Favorite.module.scss';
 import { useSelector } from 'react-redux';
-import { getFavoriteCard } from '../../redux/store';
+import { getFavoriteCard } from '../../redux/cardsRedux';
 import PageTitle from '../PageTitle/PageTitle';
 import Card from '../Card/Card';
 
