f32bd99f1542e663ce9a585f4165a68d55d56ef4
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-22 13:12:04 +0100

    change isFavorite to false

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -44,11 +44,11 @@ const initialState = {
     ],
   
     cards: [
-      { id: '1', columnId: '1', title: 'This is Going to Hurt', isFavorite: true },
+      { id: '1', columnId: '1', title: 'This is Going to Hurt', isFavorite: false },
       { id: '2', columnId: '1', title: 'Interpreter of Maladies', isFavorite: false },
-      { id: '3', columnId: '2', title: 'Lost in Space', isFavorite: true },
+      { id: '3', columnId: '2', title: 'Lost in Space', isFavorite: false },
       { id: '4', columnId: '2', title: 'Star Wars', isFavorite: false },
-      { id: '5', columnId: '3', title: 'FIFA 2023', isFavorite: true },
+      { id: '5', columnId: '3', title: 'FIFA 2023', isFavorite: false },
       { id: '6', columnId: '3', title: 'Skyrim', isFavorite: false }
     ],
 
