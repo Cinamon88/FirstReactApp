b462fda6fae546abd6c45d9444bbe9aefed46afc
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-22 13:16:22 +0100

    update style

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,22 +4,21 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleCardFavorite } from '../../redux/store';
 
-const Card = props => {
+const Card = (props) => {
     const cardId = props.cardId;
     const [favoriteValue, setFavoriteValue] = useState(props.isFavorite);
 
     const dispatch = useDispatch();
 
-    const toggleFavorite = e => {
+    const toggleFavorite = (e) => {
         e.preventDefault();
-        setFavoriteValue(!favoriteValue);
+        setFavoriteValue(favoriteValue);
         dispatch(toggleCardFavorite(cardId));
     };
 
     return (
-        <li className={styles.card}>{props.title}{""}
-            <button 
-                onClick={toggleFavorite} 
+        <li className={styles.card}>{props.title}
+            <button onClick={toggleFavorite} 
                 className={clsx(styles.button, props.isFavorite &&
                 styles.isActive)}>
                 <i className={"fa fa-star-o"} />
